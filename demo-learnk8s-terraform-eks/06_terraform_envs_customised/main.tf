7ed26f7d5344f2413987c290b6bf787ce1b3b908
Inaki Fernandez <inafev@gmail.com>
2022-02-23 19:28:31 +0100

    bigger instance type otherwise we have no room for pods

--- demo-learnk8s-terraform-eks/06_terraform_envs_customised/main.tf
+++ demo-learnk8s-terraform-eks/06_terraform_envs_customised/main.tf
@@ -1,13 +1,13 @@
 module "dev_cluster" {
   source        = "./cluster"
   cluster_name  = "dev"
-  instance_type = "t2.micro"
+  instance_type = "t2.small"
 }
 
 #module "staging_cluster" {
 #  source        = "./cluster"
 #  cluster_name  = "staging"
-#  instance_type = "t2.micro"
+#  instance_type = "t2.small"
 #}
 
 #module "production_cluster" {
